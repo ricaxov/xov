8f4157fa5f327be123973e8fc3343755a19025ac
ricaxov <103327245+ricaxov@users.noreply.github.com>
2023-06-01 09:04:48 -0300

    Update mint.cpp
    still needs to change some things

--- mint.cpp
+++ mint.cpp
@@ -1,56 +1,34 @@
-const int MOD = 998'244'353;
-template <unsigned P>
-struct Z {
+const int MOD = 998244353;
+
+template<unsigned P> struct Z {
   unsigned value;
   constexpr Z() : value(0) {}
-  template <typename T, typename = std::enable_if_t<std::is_integral<T>::value>>
-  constexpr Z(T a) : value((((long long)a % P) + P) % P) {}
-  Z& operator+=(Z rhs) {
-    value += rhs.value;
-    if (value >= P) value -= P;
-    return *this;
-  }
-  Z& operator-=(Z rhs) {
-    value += P - rhs.value;
-    if (value >= P) value -= P;
-    return *this;
-  }
-  Z& operator*=(Z rhs) {
-    value = (unsigned long long)value * rhs.value % P;
-    return *this;
-  }
-  Z& operator/=(Z rhs) { return *this *= pow(rhs, -1); }
-  Z operator+() const { return *this; }
-  Z operator-() const { return Z() - *this; }
-  bool operator==(Z rhs) const { return value == rhs.value; }
-  bool operator!=(Z rhs) const { return value != rhs.value; }
-  friend Z operator+(Z lhs, Z rhs) { return lhs += rhs; }
-  friend Z operator-(Z lhs, Z rhs) { return lhs -= rhs; }
-  friend Z operator*(Z lhs, Z rhs) { return lhs *= rhs; }
-  friend Z operator/(Z lhs, Z rhs) { return lhs /= rhs; }
-  friend std::ostream& operator<<(std::ostream& out, Z a) { return out << a.value; }
-  friend std::istream& operator>>(std::istream& in, Z& a) {
-    long long value;
-    in >> value;
-    a = Z(value);
-    return in;
-  }
+  template<typename T, typename = enable_if_t<is_integral<T>::value>> constexpr Z(T a) : value((((long long)a % P) + P) % P) {}
+  Z& operator += (Z rhs) { value += rhs.value; if(value >= P) value -= P; return *this; }
+  Z& operator -= (Z rhs) { value += P - rhs.value; if(value >= P) value -= P; return *this; }
+  Z& operator *= (Z rhs) { value = (unsigned long long)value * rhs.value % P;return *this; }
+  Z& operator /= (Z rhs) { return *this *= pow(rhs, -1); }
+  Z operator + () const { return *this; }
+  Z operator - () const { return Z() - *this; }
+  bool operator == (Z rhs) const { return value == rhs.value; }
+  bool operator != (Z rhs) const { return value != rhs.value; }
+  friend Z operator + (Z lhs, Z rhs) { return lhs += rhs; }
+  friend Z operator - (Z lhs, Z rhs) { return lhs -= rhs; }
+  friend Z operator * (Z lhs, Z rhs) { return lhs *= rhs; }
+  friend Z operator / (Z lhs, Z rhs) { return lhs /= rhs; }
+  friend ostream& operator << (ostream& os, Z a) { return os << a.value; }
+  friend istream& operator >> (istream& is, Z& a) { long long value; is >> value; a = Z(value); return is; }
 };
-template <unsigned P>
-Z<P> pow(Z<P> x, long long p) {
-  if (x == 0) {
-    return p == 0 ? 1 : 0;
-  }
+template<unsigned P> Z<P> pow(Z<P> x, long long p) {
+  if(x == 0) return (p == 0 ? 1 : 0);
   p %= P - 1;
-  if (p < 0) p += P - 1;
+  if(p < 0) p += P - 1;
   Z<P> res = 1;
-  while (p) {
-    if (p & 1) {
-      res *= x;
-    }
-    x *= x;
-    p >>= 1;
+  while(p) {
+    if(p & 1) res *= x;
+    x *= x, p >>= 1;
   }
   return res;
 }
+
 using mint = Z<MOD>;
