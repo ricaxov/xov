431f2f0342896d7c769c0a2610652441f532ac92
ricaxov <103327245+ricaxov@users.noreply.github.com>
2023-06-05 13:39:56 -0300

    did something

---
+++ codes/fexp.cpp
@@ -0,0 +1,11 @@
+const int MOD = 998'244'353;
+
+int fexp(int b, int e) {
+  b %= MOD;
+  int ans = 1;
+  while(e) {
+    if(e & 1) (ans *= b) %= MOD;
+    e >>= 1, (b *= b) %= MOD;
+  }
+  return ans;
+}
