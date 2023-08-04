dafa97ec2b1bbca09f80ec358f8b49946f30fe43
ricaxov <103327245+ricaxov@users.noreply.github.com>
2023-08-04 20:43:46 -0300

    Update seg_tree.cpp

--- codes/seg_tree.cpp
+++ codes/seg_tree.cpp
@@ -1,36 +1,36 @@
 template<typename T> class SegmentTree {
   int n;
+  V<T> SEG;
   T neutral;
-  V<T> ST;
   function<T(T,T)> merge;
 public:
-  SegmentTree(int n, T neutral, function<T(T,T)> merge) : n(n), neutral(neutral), merge(merge), ST(2 * n, neutral) {};
+  SegmentTree(int n, T neutral, function<T(T,T)> merge) : n(n), neutral(neutral), merge(merge), SEG(2 * n, neutral) {};
 
-  SegmentTree(V<T> &v, T neutral, function<T(T,T)> merge) : n(sz(v)), neutral(neutral), merge(merge), ST(2 * n, neutral) {
+  SegmentTree(V<T> &v, T neutral, function<T(T,T)> merge) : n(sz(v)), neutral(neutral), merge(merge), SEG(2 * n, neutral) {
     for(int i = 0; i < n; i++) {
-      ST[i + n] = v[i];
+      SEG[i + n] = v[i];
     }
     build();
   }
 
   void build() {
     for(int i = n - 1; i > 0; i--) {
-      ST[i] = merge(ST[2 * i], ST[2 * i + 1]);
+      SEG[i] = merge(ST[2 * i], ST[2 * i + 1]);
     }
   }
 
   T query(int l, int r) {
     T rl = neutral, rr = neutral;
     for(l += n, r += n + 1; l < r; l /= 2, r /= 2) {
-      if(l & 1) rl = merge(rl, ST[l++]);
-      if(r & 1) rr = merge(ST[--r], rr);
+      if(l & 1) rl = merge(rl, SEG[l++]);
+      if(r & 1) rr = merge(SEG[--r], rr);
     }
     return merge(rl, rr);
   }
 
   void update(int p, T val) {
-    for(ST[p += n] = val; p > 1; p /= 2) {
-      ST[p / 2] = merge(ST[min(p, p ^ 1)], ST[max(p, p ^ 1)]);
+    for(SEG[p += n] = val; p > 1; p /= 2) {
+      SEG[p / 2] = merge(SEG[min(p, p ^ 1)], SEG[max(p, p ^ 1)]);
     }
   }
 };
