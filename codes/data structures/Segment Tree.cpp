e344bef2ccfb3fdf5e6417018fa42446e38b8502
ricaxov <ricardovieiracdo@gmail.com>
2024-01-25 09:08:38 -0300

    Update Segment Tree.cpp

--- codes/data structures/Segment Tree.cpp
+++ codes/data structures/Segment Tree.cpp
@@ -1,82 +1,29 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define int long long
-
-/*
-
-A ultima versao disso daqui ta no meu codeforces
-coisas que eu acho que eu posso melhorar é fazer uma versao com lazy e sem lazy
-
-tirar as duas ultimas funcoes, declarar os trem global
-
-pra falar a verdade acho que eu preciso de inspiracao
-*/
-// Segment Tree - v.0.5.0 {{{
-template<typename T>
-class SegmentTree {
-private:
-  int N;
-  vector<T> A, S, L;
-  T neutral, unmarked;
-public:
-  explicit SegmentTree(int N, T const& neutral, T const& unmarked) : N(N), S(4 * N), L(4 * N, unmarked), neutral(neutral), unmarked(unmarked) {}
-  explicit SegmentTree(vector<T> A, T const& neutral, T const& unmarked) : N(size(A)), A(A), S(4 * N), L(4 * N, unmarked), neutral(neutral), unmarked(unmarked) { 
-    for(int i = 0; i < N; i++) {
-      set(i, i, A[i]);
-    }
-  } 
-
-  void apply(int p, int l, int r) {
-    if(L[p] != unmarked) {
-      // S[p] = L[p];
-      if (l != r) {
-        // L[2 * p] = L[p];
-        // L[2 * p + 1] = L[p];
-      }
-      L[p] = unmarked;
-    }
-  }
-
-  T get(int a, int b, int p, int l, int r) {
-    apply(p, l, r);
-    if(a <= l && r <= b) return S[p];
-    if(b < l || r < a) return neutral;
-    return get(a, b, 2 * p, l, midpoint(l, r)) + get(a, b, 2 * p + 1, midpoint(l, r) + 1, r);
-  }
-
-  T set(int a, int b, T const& x, int p, int l, int r) {
-    apply(p, l, r);
-    if(a <= l && r <= b) {
-      L[p] = x;
-      apply(p, l, r);
-      return S[p];
-    }
-    if(b < l || r < a) return S[p];
-    return S[p] = set(a, b, x, 2 * p, l, midpoint(l, r)) + set(a, b, x, 2 * p + 1, midpoint(l, r) + 1, r);
-  }
-
-  T get(int a, int b) { return get(a, b, 1, 0, N - 1); }
-  T set(int a, int b, T const& x) { return set(a, b, x, 1, 0, N - 1); }
+using Node = iii;
+const int MAXN = 1e5 + 5, INF = 1e18;
+ 
+enum Info {
+  Q = 0,
+  M = 1,
+  G = 2
 };
-//}}}
-
-struct Node {
-  int A;
-  Node() : A(0) {}
-  Node(int A) : A(A) {}
-  Node operator + (Node N) const {
-    return A + N.A;
-  }
-  bool operator != (Node N) const {
-    return A != N.A;
-  }
-};
-
-const Node neutral, unmarked;
-
-// auto main() -> signed {
-//   ios_base::sync_with_stdio(false); cin.tie(nullptr);
-
-
-// }
+ 
+int N;
+V<int> A;
+V<Node> T(4 * MAXN);
+ 
+Node merge(Node L, Node R) {
+  if(L[M] < R[M]) return {L[Q], L[M], gcd(L[G], R[G])};
+  else if(L[M] > R[M]) return {R[Q], R[M], gcd(R[G], L[G])};
+  return {L[Q] + R[Q], L[M], gcd(R[G], L[G])};
+}
+Node build(int p = 1, int l = 0, int r = N - 1) {
+  if(l == r) return T[p] = {1, A[l], A[l]};
+  return T[p] = merge(build(2 * p, l, midpoint(l, r)), 
+                      build(2 * p + 1, midpoint(l, r) + 1, r)); 
+}
+Node query(int x, int y, int p = 1, int l = 0, int r = N - 1) {
+  if(x <= l && r <= y) return T[p];
+  if(l > y || r < x) return {0, INF, 0};
+  return merge(query(x, y, 2 * p, l, midpoint(l, r)),
+               query(x, y, 2 * p + 1, midpoint(l, r) + 1, r)); 
+}
