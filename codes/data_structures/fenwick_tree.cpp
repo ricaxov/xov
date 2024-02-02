fe7f6abf3ce341f0d6e0986ce5850c28755d6c90
ricaxov <103327245+ricaxov@users.noreply.github.com>
2024-02-02 08:37:08 -0300

    did something

--- codes/data_structures/fenwick_tree.cpp
+++ codes/data_structures/fenwick_tree.cpp
@@ -1,23 +1,20 @@
 // Fenwick Tree {{{
 struct FenwickTree {
   int N;
-  vector<int> data;
-  explicit FenwickTree(int N) : N(N), data(N) {}
-
+  vector<int> BIT;
+  explicit FenwickTree(int N) : N(N), BIT(N, 0) {}
   void add(int idx, int delta) {
     for(int i = idx; i < N; i |= (i + 1)) {
-      data[i] += delta;
+      BIT[i] += delta;
     }
   }
-
   int sum(int r) {
     int ret = 0;
-    for (int i = r; i >= 0; i &= (i + 1), i--) {
-      ret += data[i];
+    for(int i = r; i >= 0; i &= (i + 1), i--) {
+      ret += BIT[i];
     }
     return ret;
   }
-
   int sum(int l, int r) {
     return sum(r) - sum(l - 1);
   }
