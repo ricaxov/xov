9cac4a64141d438096962b737bc6869d8d562733
Ricardo Vieira <103327245+ricaxov@users.noreply.github.com>
2024-03-01 10:00:56 -0300

    mo.cpp

--- codes/data_structures/mo's algorithm.cpp
+++ codes/data_structures/mo's algorithm.cpp
@@ -1,35 +1,34 @@
 const int MAXN = 1e5 + 5;
  
-int cur;
-V<int> v, occ(MAXN, 0);
+int N;
+vector<int> A, M(MAXN, 0);
  
 struct Query {
   int l, r, p;
 };
- 
-void add(int x) {
-
-}
-void rem(int x) {
-  
-}
- 
-V<int> mo(V<Query> queries) {
-  int bs = sqrt(sz(v));
-  sort(all(queries), [bs](const Query& x, const Query& y) {
-    int bx = x.l / bs, by = y.l / bs;
-    if(bx != by) return bx < by;
-    return (bx % 2 == 0 ? x.r < y.r : y.r < x.r);
+vector<int> mo(vector<Query> Q) {
+  int sum = 0;
+  auto add = [&](int x) -> void {
+    
+  };
+  auto rem = [&](int x) -> void {
+    
+  };
+  int len = sqrt(size(A));
+  sort(begin(Q), end(Q), [len](Query const& x, Query const& y) {
+    int xlen = x.l / len, ylen = y.l / len;
+    if(xlen != ylen) return xlen < ylen;
+    return (xlen % 2 == 0 ? x.r < y.r : y.r < x.r);
   });
   int bl = 0, br = -1;
-  V<int> ans(sz(queries));
-  for(int i = 0; i < sz(queries); i++) {
-    auto [l, r, p] = queries[i];
+  vector<int> ans(size(Q));
+  for(int i = 0; i < size(Q); i++) {
+    auto [l, r, p] = Q[i];
     while (br < r) add(++br);
     while (br > r) rem(br--);
     while (bl < l) rem(bl++);
     while (bl > l) add(--bl);
-    ans[p] = cur;
+    ans[p] = sum;
   }
   return ans;
 }
