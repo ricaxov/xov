2de318027b9aaeed63c7ea9f9eb3452e39c605eb
Ricardo Vieira <103327245+ricaxov@users.noreply.github.com>
2024-03-01 14:40:12 -0300

    did something

--- codes/data_structures/lazy_seg_tree.cpp
+++ codes/data_structures/lazy_seg_tree.cpp
@@ -1,55 +1,44 @@
-// needs fixing
-template<typename T> class SegmentTree {
-private:
-  int n;
-  T neutral;
-  vector<T> SEG, LAZY;
+template<typename T>/*{{{*/
+struct LazySegmentTree {
+  int N;
+  vector<T> data, lazy;
+  explicit LazySegmentTree(int N) : N(N), data(4 * N), lazy(4 * N, 0) {}
+ 
   T merge(T a, T b) {
     return a + b;
   }
-  void apply(int p, bool c) {
-    if(LAZY[p]) {
-      SEG[p] += LAZY[p];
-      if(c) LAZY[2 * p] += LAZY[p], LAZY[2 * p + 1] += LAZY[p];
-      LAZY[p] = 0;
+ 
+  void apply(int p, int l, int r) {
+    if(lazy[p] != 0) {
+      data[p] += lazy[p];
+      if(l != r) {
+        lazy[2 * p] += lazy[p];
+        lazy[2 * p + 1] += lazy[p];
+      }
+      lazy[p] = 0;
     }
   }
-  void build(int l, int r, int p, vector<T> const& v) {
-    if(l == r) {
-      SEG[p] = v[l]; return;
+ 
+  T set(int x, int y, T const& v, int p, int l, int r) {
+    apply(p, l, r);
+    if(x <= l && r <= y) {
+      lazy[p] += v;
+      apply(p, l, r);
+      return data[p];
     }
-    int m = (l + r) / 2;
-    build(l, m, 2 * p, v);
-    build(m + 1, r, 2 * p + 1, v);
-    SEG[p] = merge(SEG[2 * p], SEG[2 * p + 1]);
+    if(y < l || r < x) return data[p];
+    int m = midpoint(l, r);
+    return data[p] = merge(set(x, y, v, 2 * p, l, m), set(x, y, v, 2 * p + 1, m + 1, r));
   }
-  T query(int l, int r, int p, int a, int b) {
-    apply(p, r - l);
-    if(a > r || b < l) return neutral;
-    if(l >= a && r <= b) return SEG[p];
-    int m = (l + r) / 2;
-    return merge(query(l, m, 2 * p, a, b), query(m + 1, r, 2 * p + 1, a, b));
+ 
+  T get(int x, int y, int p, int l, int r) {
+    apply(p, l, r);
+    if(x <= l && r <= y) return data[p];
+    if(y < l || r < x) return 0;
+    int m = midpoint(l, r);
+    return merge(get(x, y, 2 * p, l, m), get(x, y, 2 * p + 1, m + 1, r));
   }
-  void update(int l, int r, int p, int a, int b, T x) {
-    apply(p, r - l);
-    if(a > r || b < l) return;
-    if(l >= a && r <= b) {
-      LAZY[p] = x;
-      apply(p, r - l);
-      return;
-    }
-    int m = (l + r) / 2;
-    update(l, m, 2 * p, a, b, x);
-    update(m + 1, r, 2 * p + 1, a, b, x);
-    SEG[p] = merge(SEG[2 * p], SEG[2 * p + 1]);
-  }
-public:
-  SegmentTree() {}
-  SegmentTree(int n) : n(n), SEG(4 * n), LAZY(4 * n, 0) {};
-  SegmentTree(vector<T> const& v) : n((int)v.size()), SEG(4 * n), LAZY(4 * n, 0) {
-    neutral = 0;
-    build(0, n - 1, 1, v);
-  }
-  T query(int x, int y) { return query(0, n - 1, 1, x, y); }
-  void update(int x, int y, T v) { update(0, n - 1, 1, x, y, v); }
-};
+ 
+  T set(int x, int y, T v) { return set(x, y, v, 1, 0, N - 1); }
+  T get(int x, int y) { return get(x, y, 1, 0, N - 1); }
+};/*}}}*/
