54572d317416518e8e099b585504d00c5d7600d2
Ricardo Vieira <103327245+ricaxov@users.noreply.github.com>
2024-04-16 13:43:29 -0300

    did something

--- codes/base/debug.cpp
+++ codes/base/debug.cpp
@@ -1,4 +1,4 @@
 // Debug {{{
 #define var(x) "[", #x, " = ", x, "]"
-template<class...X> void dbg(X const&...x) { ((cout << x), ...); cout << "\n"; }
+template<typename...X> void dbg(X const&...x) { ((cout << x), ...); cout << "\n"; }
 //}}}
