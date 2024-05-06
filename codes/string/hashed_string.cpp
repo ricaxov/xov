a5a6262a406cdac622aa6f5861f654162c17dfdd
Ricardo Vieira <103327245+ricaxov@users.noreply.github.com>
2024-05-06 16:19:02 -0300

    did something

--- codes/string/hashed_string.cpp
+++ codes/string/hashed_string.cpp
@@ -1,24 +1,30 @@
-const int M1 = 1000015553, M2 = 1000028537;
-mt19937 rng((int)chrono::steady_clock::now().time_since_epoch().count());
-const int B1 = uniform_int_distribution<int>(356, M1 - 1)(rng), B2 = uniform_int_distribution<int>(356, M2 - 1)(rng);
- 
-// Hashed String {{{ 
+// Hashed String {{{
 struct HashedString {
+  static const int M = (1LL << 61) - 1;
+  static const int B;
+  static vector<int> P;
+
   int N;
-  string S;
-  vector<int> H1, P1, H2, P2;
-  explicit HashedString() : N(0) {}
-  explicit HashedString(string const& S) : N(size(S)), S(S), H1(N, S[0]), P1(N, 1), H2(N, S[0]), P2(N, 1) {
-    for(int i = 1; i < N; i++) {
-      P1[i] = (P1[i - 1] * B1) % M1, P2[i] = (P2[i - 1] * B2) % M2;
-      H1[i] = (H1[i - 1] * B1 + S[i]) % M1, H2[i] = (H2[i - 1] * B2 + S[i]) % M2;
+  vector<int> H;
+
+  __int128 mul(int a, int b) { return (__int128)a * b; }
+  int mod_mul(int a, int b) { return mul(a, b) % M; }
+
+  explicit HashedString(string const& S) : H(size(S) + 1), N(size(S)) {
+    while (size(P) < size(S) + 1) P.push_back(mod_mul(P.back(), B));
+
+    H[0] = 0;
+    for (int i = 0; i < size(S); i++) {
+      H[i + 1] = (mul(H[i], B) + S[i]) % M;
     }
   }
-  int get(int l, int r) {
-    if(l == 0) return ((H1[r] << 30) ^ (H2[r]));
-    int R1 = (((H1[r] - (H1[l - 1] * P1[r - l + 1])) % M1) + M1) % M1;
-    int R2 = (((H2[r] - (H2[l - 1] * P2[r - l + 1])) % M2) + M2) % M2;
-    return ((R1 << 30) ^ (R2));
+
+  int get_hash(int l, int r) {
+    int R = H[r + 1] - mod_mul(H[l], P[r - l + 1]);
+    return (R + M) % M;
   }
 };
+vector<int> HashedString::P{1};
+mt19937 rng((int)chrono::steady_clock::now().time_since_epoch().count());
+const int HashedString::B = uniform_int_distribution<int>(0, M - 1)(rng);
 //}}}
