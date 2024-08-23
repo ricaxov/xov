bef8ce1f894a48d5bf44e8d03f667b87b1776e8b
Ricardo Vieira <103327245+ricaxov@users.noreply.github.com>
2024-08-23 10:05:14 -0300

    did something

--- codes/geometry/base.cpp
+++ codes/geometry/base.cpp
@@ -1,4 +1,4 @@
-// Geometry Template (v0.5.5 - 22/08/2024) (Jotinha, ricaxov, UmMainAkali) {{{
+// Geometry Template (v0.6.0 - 23/08/2024) (Jotinha, ricaxov, UmMainAkali) {{{
 const long double EPS = 1e-9;
 const long double PI = acosl(-1.0);
   
@@ -101,10 +101,12 @@ long double area(Point<T> const& a, Point<T> const& b, Point<T> const& c) {
   return abs(sarea(a, b, c));
 }
  
+/* ccw (a must be in radians) */
+
 template<typename T>
-Point<T> rot(Point<T> const& p, long double a) { // ccw
-  return Point<T>{p.x * cos(a) - p.y * sin(a),
-                  p.x * sin(a) + p.y * cos(a)};
+Point<T> rot(Point<T> const& p, long double a) {
+  return Point<T>{p.x * cosl(a) - p.y * sinl(a),
+                  p.x * sinl(a) + p.y * cosl(a)};
 }
  
 template<typename T>
@@ -275,7 +277,7 @@ pair<int, int> pick(Poly<T> P) {
 template<typename T>
 long double angle(Point<T> const& a, Point<T> const& o, Point<T> const& b) {
   long double ang = (a-o) * (b-o) / norm(a-o) / norm(b-o);
-  return acos(max(min(ang, (long double)1), (long double)-1));
+  return acosl(max(min(ang, (long double)1), (long double)-1));
 }
 
 /* must be integral T */
@@ -291,18 +293,25 @@ Point<T> reduce(Point<T> const& a) {
 }
 
 template<typename T>
-void polar_sort(vector<Point<T>>& P) { // ccw
+void polar_sort(vector<Point<T>>& P) {
   auto sign = [&](T const& a) -> int {
     return (a > EPS) - (a < -EPS);
   };
-
   auto half = [&](Point<T> const& a) -> int {
     return (!eq(a.y, T()) ? sign(a.y) : -sign(a.x));
   };
-
   sort(begin(P), end(P), [&](Point<T> const& a, Point<T> const& b) {
     int ha = half(a), hb = half(b);
     return (ha == hb ? (a^b) > 0 : ha < hb);
   });
 }
+
+/* ang(c,d) <= ang(a,b) (both have e as a fixed point) */
+
+template<typename T>
+bool angle_less(Point<T> const& a, Point<T> const& b, Point<T> const& c, Point<T> const& d, Point<T> const& e) {
+  Point p1{(a-e)*(b-e), abs((a-e)^(b-e))};
+  Point p2{(c-e)*(d-e), abs((c-e)^(d-e))};
+  return (p2^p1) >= EPS;
+};
 //}}}
