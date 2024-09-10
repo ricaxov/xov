dc4e13b3f7a41ac63b5bd695fa5121f2b2d9eb85
Ricardo Vieira <103327245+ricaxov@users.noreply.github.com>
2024-09-10 11:32:48 -0300

    did something

--- codes/miscellaneous/contest.sh
+++ codes/miscellaneous/contest.sh
@@ -1,7 +1,5 @@
-# Make Contest Files (v0.5.0 - 06/09/2024) {{{
-echo "Digite quem serão os participantes:"
-read contestants
-
+# Contest Files (v0.7.0 - 10/09/2024) {{{
+# chmod +x contest.sh && ./contest.sh
 echo "Digite o ID do contest que será feito:"
 read contest_id
 
@@ -9,26 +7,32 @@ response=$(curl -s "https://codeforces.com/api/contest.standings?contestId=$cont
 contest_name=$(echo "$response" | jq -r '.result.contest.name')
 
 if [ "$contest_name" = "null" ]; then
-  echo "Não foi possível obter o nome do contest. Verifique o contest_id e tente novamente."
+  echo "Não foi possível obter o nome do contest. Verifique o ID da competição e tente novamente."
   exit 1
 fi
 
+amount=$(echo "$response" | jq '.result.problems | length')
+
 echo "Nome do contest: $contest_name"
+echo "Quantidade de problemas: $amount"
+
 dir_name=$(echo "$contest_name" | sed 's/ /_/g')
 mkdir -p "$dir_name"
 
 cd "$dir_name" || exit
 xdg-open "https://codeforces.com/gym/$contest_id"
 
-IFS=' '
-contestants_array=($contestants)
-
 touch stress.sh
 touch makefile
 code makefile
 
-for contestant in "${contestants_array[@]}"; do
-  touch "$contestant.cpp"
-  code "$contestant.cpp"
+for letter in {a..z}; do
+  if [ "$amount" -eq 0 ]; then
+    break
+  else
+    touch "$letter.cpp"
+    code "$letter.cpp"
+    ((amount--))
+  fi
 done
 #}}}
