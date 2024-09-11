809d7f8ef5c1ed4296c91b31a8ab96830ba71ca3
Ricardo Vieira <103327245+ricaxov@users.noreply.github.com>
2024-09-11 11:23:59 -0300

    asd

--- codes/data_structures/dsu.cpp
+++ codes/data_structures/dsu.cpp
@@ -2,18 +2,21 @@
 struct DSU {
   vector<int> P, S;
   explicit DSU(int N) : P(N, -1), S(N, 1) {};
+  
   int leader(int v) {
-    if(P[v] == -1) return v;
+    if (P[v] == -1) return v;
     return P[v] = leader(P[v]);
   }
+  
   int merge(int a, int b) {
     a = leader(a), b = leader(b);
-    if(a != b) {
-      if(S[a] < S[b]) swap(a, b);
+    if (a != b) {
+      if (S[a] < S[b]) swap(a, b);
       P[b] = a, S[a] += S[b];
     }
     return a;
   }
+
   bool same(int a, int b) {
     return leader(a) == leader(b);
   }
