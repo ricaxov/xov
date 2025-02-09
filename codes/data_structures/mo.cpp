a82c9ded4d9c2efec960c5bd4553d85f60e4d569
Ricardo Vieira <103327245+ricaxov@users.noreply.github.com>
2025-02-09 20:08:06 -0300

    Update mo.cpp

--- codes/data_structures/mo.cpp
+++ codes/data_structures/mo.cpp
@@ -1,34 +1,29 @@
-const int MAXN = 1e5 + 5;
- // terminar isso daqui dps
-int N;
-vector<int> A, M(MAXN, 0);
- 
-struct Query {
-  int l, r, p;
-};
-vector<int> mo(vector<Query> Q) {
-  int sum = 0;
-  auto add = [&](int x) -> void {
-    
-  };
-  auto rem = [&](int x) -> void {
-    
-  };
-  int len = sqrt(size(A));
-  sort(begin(Q), end(Q), [len](Query const& x, Query const& y) {
-    int xlen = x.l / len, ylen = y.l / len;
-    if(xlen != ylen) return xlen < ylen;
-    return (xlen % 2 == 0 ? x.r < y.r : y.r < x.r);
-  });
-  int bl = 0, br = -1;
-  vector<int> ans(size(Q));
-  for(int i = 0; i < size(Q); i++) {
-    auto [l, r, p] = Q[i];
-    while (br < r) add(++br);
-    while (br > r) rem(br--);
-    while (bl < l) rem(bl++);
-    while (bl > l) add(--bl);
-    ans[p] = sum;
+// Mo's Algorithm {{{
+const int BLK = 400;
+struct MoQuery {
+  int l, r, idx;
+  bool operator < (MoQuery const& o) const {
+    if (l/BLK != o.l/BLK) return l < o.l;
+    if (l/BLK % 2 == 0) return r < o.r;
+    else return r > o.r;
   }
-  return ans;
+};
+//}}}
+
+auto add = [&](int x) {
+    
+};
+auto rem = [&](int x) {
+  
+};
+
+int bl = 0, br = -1;
+vector<bool> ans(size(Q));
+for (int i = 0; i < size(Q); i++) {
+  auto [l, r, p] = Q[i];
+  while (br < r) add(++br);
+  while (br > r) rem(br--);
+  while (bl < l) rem(bl++);
+  while (bl > l) add(--bl);
+  ans[p] = ANSWER;
 }
