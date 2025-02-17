c4fb5e5eb0491fdf440818314bb0423ab3b318e5
Ricardo Vieira <103327245+ricaxov@users.noreply.github.com>
2025-02-17 15:26:51 -0300

    Update combinatorics.cpp

--- codes/mathematics/combinatorics.cpp
+++ codes/mathematics/combinatorics.cpp
@@ -1,23 +1,84 @@
-auto fexp = [&](auto fexp, int b, int e) -> int {
-  if(e == 0) return 1;
-  int h = fexp(fexp, b, e / 2);
-  (h *= h) %= MOD;
-  if(e % 2 != 0) (h *= b) %= MOD;
-  return h;
+// Modular Arithmetic {{{
+template<unsigned P>
+struct Z {
+  unsigned value;
+
+  Z() : value(0) {}
+  
+  Z(int x) : value(((x%P)+P)%P) {}
+  
+  Z& operator += (Z m) {
+    value += m.value;
+    if (value >= P) value -= P;
+    return *this;
+  }
+
+  Z& operator -= (Z m) {
+    value += P - m.value;
+    if (value >= P) value -= P;
+    return *this;
+  }
+
+  Z& operator *= (Z m) {
+    (value *= m.value) %= P;
+    return *this;
+  }
+
+  Z& operator /= (Z m) { 
+    return *this *= pow(m, -1);
+  }
+
+  Z operator + () const { return *this; }
+
+  Z operator - () const { return Z() - *this; }
+
+  bool operator == (Z m) const { return value == m.value; }
+
+  bool operator != (Z m) const { return value != m.value; }
+
+  friend Z operator + (Z l, Z m) { return l += m; }
+
+  friend Z operator - (Z l, Z m) { return l -= m; }
+
+  friend Z operator * (Z l, Z m) { return l *= m; }
+
+  friend Z operator / (Z l, Z m) { return l /= m; }
+ 
+  friend ostream& operator << (ostream& os, Z a) { os << a.value; return os; }
+  
+  friend istream& operator >> (istream& is, Z& a) { int x; is >> x; a = Z(x); return is; }
 };
 
-auto multinv = [&](int x) -> int {
-  return fexp(fexp, x, MOD - 2);
-};
+template<unsigned P>
+Z<P> pow(Z<P> b, int e) {
+  if (b == 0) return e == 0 ? 1 : 0;
+  e %= P-1;
+  if (e < 0) e += P-1;
 
-V<int> fact(MAXN), ifact(MAXN);
-fact[0] = 1, ifact[0] = 1;
-for(int i = 1; i < MAXN; i++) {
-  fact[i] = (i * fact[i - 1]) % MOD;
-  ifact[i] = multinv(fact[i]);
+  Z<P> ans = 1;
+  while (e) {
+    if (e & 1) ans *= b;
+    b *= b, e >>= 1;
+  }
+  return ans;
 }
+//}}}
 
-auto choose = [&](int n, int k) -> int {
-  if(n < k) return 0;
-  return (((fact[n] * ifact[n - k]) % MOD) * ifact[k]) % MOD;
+// Combinatorics {{{
+template <unsigned P>
+struct Combinatorics {
+  vector<Z<P>> fact, ifact;
+
+  explicit Combinatorics(int N) : fact(N), ifact(N) {
+    fact[0] = 1;
+    for (int i = 1; i < N; i++) fact[i] = fact[i-1] * i;
+    ifact[N-1] = 1 / fact[N-1];
+    for (int i = N-1; i-1 >= 0; i--) ifact[i-1] = ifact[i] * i;
+  }
+
+  Z<P> C(int n, int k) const {
+    if (k < 0 || n < k) return 0;
+    return fact[n] * ifact[k] * ifact[n-k];
+  }
 };
+//}}}
