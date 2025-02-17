19ea08deb2b32b83c4313ca52db97bcbb9618582
Ricardo Vieira <103327245+ricaxov@users.noreply.github.com>
2025-02-17 15:30:27 -0300

    Update sieve_&_prime_factors.cpp

--- codes/mathematics/sieve_&_prime_factors.cpp
+++ codes/mathematics/sieve_&_prime_factors.cpp
@@ -1,36 +1,33 @@
 // Sieve of Eratosthenes {{{
 vector<int> sieve(int N) {
-  vector<bool> prime(N, 1);
+  vector<bool> prime(N+1, 1);
   prime[0] = prime[1] = 0;
 
-  vector<int> P;
-  for(int i = 2; i < N; i++) {
-    if(prime[i] == 1) {
-      P.emplace_back(i);
-      for(int j = i * i; j < N; j += i) {
+  vector<int> primes;
+  for (int i = 2; i <= N; i++) {
+    if (prime[i]) {
+      primes.emplace_back(i);
+      for (int j = 2*i; j <= N; j += i) {
         prime[j] = 0;
       }
     }
   }
-  return P;
+  return primes;
 }
 //}}}
 
-const int MAXN = 2e5 + 5;
-auto primes = sieve(MAXN);
-
-// Prime Factorization {{{
-vector<pair<int, int>> factorize(int N) {
+// Factors {{{
+vector<pair<int, int>> factors(int N) {
   vector<pair<int, int>> F;
-  for(auto p : primes) {
-    if(p * p > N) break;
-    if(N % p == 0) {
+  for (auto p : primes) {
+    if (p*p > N) break;
+    if (N % p == 0) {
       F.emplace_back(p, 0);
       auto &[a, b] = F.back();
-      while(N % a == 0) N /= a, b++;
+      while (N % a == 0) N /= a, b += 1;
     }
   }
-  if(N > 1) F.emplace_back(N, 1);
+  if (N != 1) F.emplace_back(N, 1);
   return F;
 }
 //}}}
